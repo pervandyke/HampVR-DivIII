c616d5a273703df10b5392283298e8283d3f01b3
Per Van Dyke <pav17@hampshire.edu>
2020-11-18 17:04:27 -0500

    set up initial missile

---
+++ HampVR-src/Assets/MainAssets/Scripts/Weapons/MissileScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileScript : MonoBehaviour
+{
+    public float speed;
+    public float turningSpeed;
+    public int damage;
+    public string missileType;
+    public GameObject target;
+    private float timer = 15;
+
+    private void FixedUpdate()
+    {
+        //rotate to face target
+        Quaternion targetRotation = Quaternion.LookRotation(target.transform.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.fixedDeltaTime * turningSpeed);
+        //apply forward force
+
+        timer -= Time.fixedDeltaTime;
+        if (timer <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //playParticle();
+        if (other.gameObject.layer == 10)
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                print("Hit player");
+                other.gameObject.GetComponentInChildren<PlayerController>().TakeHit(damage, gameObject);
+                Destroy(gameObject);
+            }
+            if (other.gameObject.tag == "Enemy")
+            {
+                print("Hit enemy");
+                other.gameObject.GetComponent<IEnemy>().TakeDamage(damage);
+                Destroy(gameObject);
+            }
+        }
+
+        //collisionflag = true;
+    }
+
+}
