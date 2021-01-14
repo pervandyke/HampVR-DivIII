831db5f15792c7e2894515034794e37f2db98ee1
Per Van Dyke <pav17@hampshire.edu>
2021-01-14 13:40:31 -0600

    removed old unused map gen code

--- HampVR-src/Assets/MainAssets/Scripts/MapGen/GenerateHexGrid.cs
+++ HampVR-src/Assets/MainAssets/Scripts/MapGen/GenerateHexGrid.cs
@@ -27,65 +27,6 @@ public class GenerateHexGrid : MonoBehaviour
         //GenerateMap(CreateFromJSON("JSONS/TestMapData"));
     }
 
-    private void CreateGrid()
-    {
-        PlaceHexOld();
-        if (layers > 0)
-        {
-            for (int i = 0; i < layers; i++)
-            {
-                int hexesPlaced = 0;
-                currentPosition.x = currentPosition.x - hexHeight;
-                PlaceHexOld();
-                hexesPlaced++;
-                for (int j = 0; j <= i; j++)
-                {
-                    currentPosition.x = currentPosition.x + (hexHeight/2);
-                    currentPosition.z = currentPosition.z + (hexWidth * .75f);
-                    PlaceHexOld();
-                    hexesPlaced++;
-                }
-                for (int j = 0; j <= i; j++)
-                {
-                    currentPosition.x = currentPosition.x + (hexHeight);
-                    PlaceHexOld();
-                    hexesPlaced++;
-                }
-                for (int j = 0; j <= i; j++)
-                {
-                    currentPosition.x = currentPosition.x + (hexHeight / 2);
-                    currentPosition.z = currentPosition.z - (hexWidth * .75f);
-                    PlaceHexOld();
-                    hexesPlaced++;
-                }
-                for (int j = 0; j <= i; j++)
-                {
-                    currentPosition.x = currentPosition.x - (hexHeight / 2);
-                    currentPosition.z = currentPosition.z - (hexWidth * .75f);
-                    PlaceHexOld();
-                    hexesPlaced++;
-                }
-                for (int j = 0; j <= i; j++)
-                {
-                    currentPosition.x = currentPosition.x - (hexHeight);
-                    PlaceHexOld();
-                    hexesPlaced++;
-                }
-                for (int j = 0; j <= i; j++)
-                {
-                    currentPosition.x = currentPosition.x - (hexHeight / 2);
-                    currentPosition.z = currentPosition.z + (hexWidth * .75f);
-                    if (hexesPlaced == 6 * (i + 1))
-                    {
-                        break;
-                    }
-                    PlaceHexOld();
-                    hexesPlaced++;
-                }
-            }
-        }
-    }
-
     private void GenerateMap(MapObject mapData)
     {
         foreach(HexPiece hex in mapData.tiles)
@@ -99,13 +40,6 @@ public class GenerateHexGrid : MonoBehaviour
     {
         var jsonTextFile = Resources.Load<TextAsset>(path);
         return JsonUtility.FromJson<MapObject>(jsonTextFile.ToString());
-    }
-
-    private void PlaceHexOld()
-    {
-        Quaternion rotation = new Quaternion();
-        rotation.eulerAngles = Vector3.zero;
-        GameObject HexInstance = Instantiate(Resources.Load<GameObject>("Prefabs/Hex"), currentPosition, rotation) as GameObject;
     }
 
     private void PlaceHex(int hexX, int hexZ, string tileType)
