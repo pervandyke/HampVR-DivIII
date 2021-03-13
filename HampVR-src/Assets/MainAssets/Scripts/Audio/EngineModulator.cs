9af642da25c09ef95ae5401594de656075819daa
Per Van Dyke <pav17@hampshire.edu>
2021-03-13 14:53:18 -0500

    dynamic engine audio v1

---
+++ HampVR-src/Assets/MainAssets/Scripts/Audio/EngineModulator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+public class EngineModulator : MonoBehaviour
+{
+    public StudioEventEmitter engineEmitter;
+    public PlayerController PC;
+
+
+
+    private void Update()
+    {
+        float speedPercentage = Mathf.InverseLerp(0, PC.maxSpeed, PC.RB.velocity.magnitude);
+        engineEmitter.SetParameter("RPM", 2000 * speedPercentage);
+    }
+}
