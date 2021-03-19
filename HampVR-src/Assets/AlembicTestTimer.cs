232899ec1eae8504f29727858c415b19371ce826
Per Van Dyke <pav17@hampshire.edu>
2021-03-19 13:47:42 -0400

    explosion experiments

---
+++ HampVR-src/Assets/AlembicTestTimer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlembicTestTimer : MonoBehaviour
+{
+    public 
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
