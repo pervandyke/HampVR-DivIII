8fda21b1874e13cacfa5b0ad2ed54e2c34c79e99
Per Van Dyke <pav17@hampshire.edu>
2021-04-03 14:10:11 -0400

    quit button

---
+++ HampVR-src/Assets/MainAssets/Scripts/UI/QuitScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuitScript : MonoBehaviour
+{
+    public float timerDefault;
+    public bool buttonHeld;
+    private float buttonTimer;
+    public UIController UIC;
+
+    private void Start()
+    {
+        buttonTimer = timerDefault;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        buttonHeld = true;
+        gameObject.GetComponent<Image>().color = new Color(0.7843137f, 0.7843137f, 0.7843137f);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        buttonHeld = false;
+        gameObject.GetComponent<Image>().color = Color.white;
+    }
+
+    private void Update()
+    {
+        if (buttonHeld)
+        {
+            buttonTimer -= Time.deltaTime;
+            if (buttonTimer <= 0)
+            {
+                buttonTimer = timerDefault;
+                buttonHeld = false;
+                print("Quitting");
+                Application.Quit();
+            }
+        }
+        else if (!buttonHeld && buttonTimer != timerDefault)
+        {
+            buttonTimer = timerDefault;
+        }
+    }
+}
